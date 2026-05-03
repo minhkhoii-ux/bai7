3259985c8dd72f1ba6f4adbc02bb1a28cb9dd4c1
Khôi Hoàng <khoihoang252007@gmail.com>
2026-05-03 22:44:21 +0700

    set up CI

---
+++ src/main/java/TestService.java
@@ -0,0 +1,3 @@
+public class TestService {
+    private int Biến_Sai_Dinh_Dang = 10;
+}
---
+++ src/test/java/TestApp.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestApp {
+    @Test
+    void demoTest() {
+        // Test này luôn đúng để kiểm tra CI hoạt động
+        assertTrue(true);
+    }
+}
